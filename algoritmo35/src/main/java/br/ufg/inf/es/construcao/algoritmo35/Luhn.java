b61318fb5189e55382f8664cb7e722ed44ac6f80
jhordan <jhordangabriel@hotmail.com>
2015-11-07 20:49:29 -0200

    adicionando classes vazias

---
+++ algoritmo35/src/main/java/br/ufg/inf/es/construcao/algoritmo35/Luhn.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.es.construcao.algoritmo35;
+
+/**
+ *
+ * @author Jhordan
+ */
+public class Luhn {
+    
+}
